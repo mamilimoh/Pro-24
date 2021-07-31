f03b692878dc09e2c16629d53706e87b23baf8fa
mamilimoh <77106421+mamilimoh@users.noreply.github.com>
2021-07-31 20:17:43 +0530

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,27 @@
+class Iron {
+    constructor(x, y, height,angle) {
+      var options = {
+          'restitution':0.8,
+          'friction':3,
+          'density':30
+      }
+      this.body = Bodies.rectangle(x, y, 20, height, options);
+      this.width = 40;
+      this.height = 30;
+      Matter.Body.setAngle(this.body, angle);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(4);
+      stroke("red");
+      fill("red");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  }
---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var stone;
+var iron;
+var rubber;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    stone = new Stone(10,100);
+    iron=new Iron(810,260,300,PI/2);
+    rubber=new Rubber(200,200,30);
+
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    stone.display();
+    iron.display();
+    rubber.display();
+   // ellipse(200,200,10,10);
+
+    
+ 
+}
